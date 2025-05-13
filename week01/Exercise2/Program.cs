0200baf9bbb400340f481cb11bb90d6bb453963f
ezekieledem <ezekieledem222@gmail.com>
2025-05-13 15:33:17 +0100

    working on my week 1 assignment almost done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+       Console.Write("What is your grade: ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        string letter = "";
+
+        
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+
+        {
+           letter = "B";
+    
+        }
+        else if (percentage >=70)
+        {
+            letter = "C";
+        }
+        else if (percentage >=60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+
+            Console.WriteLine($"letter grade is: {letter}");
+        }
+        if (percentage >=70)
+        {
+            Console.WriteLine("Congratulation to you bro");
+        }
+
+        else
+        {
+            Console.WriteLine("Try more guys,almost there.");
+        }
+
     }
+    
+
 }
